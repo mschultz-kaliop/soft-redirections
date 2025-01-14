ad72d9a85194bdae715c22b017db975d9d8a7d1a
Melvin Schultz <mschultz@kaliop.com>
2025-01-14 15:26:21 +0100

    rename StrapiDataSource into StrapiDatasource

--- backend/src/scripts/addRedirectionMock.ts
+++ backend/src/scripts/addRedirectionMock.ts
@@ -1,5 +1,5 @@
 import PostgresDatasource from '../datasource/postgres/PostgresDatasource'
-import StrapiDataSource from '../datasource/strapi/StrapiDataSource'
+import StrapiDatasource from '../datasource/strapi/StrapiDatasource'
 
 import { ARTICLE_ROUTE_PREFIX, ARTICLE_CONTENT_TYPE_PLURAL } from '../api/Article'
 import { Article } from '../types/article'
@@ -12,7 +12,7 @@ const run = async () => {
   const postgresDataSource = new PostgresDatasource()
   await postgresDataSource.authenticate()
   await postgresDataSource.models.UrlsRedirections.sync({ alter: true })
-  const strapiDataSource = new StrapiDataSource()
+  const strapiDataSource = new StrapiDatasource()
 
   try {
     if (process.argv.includes('--clear')) {
